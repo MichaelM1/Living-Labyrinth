56bced62dfcca10eb3955561a3fa27a425396564
derxiang <52870745+derxiang@users.noreply.github.com>
2019-08-09 23:45:23 -0700

    Added Menu Screen

--- main.py
+++ main.py
@@ -8,16 +8,13 @@ pygame.init()
 
 # music
 pygame.mixer.init()
-pygame.mixer_music.load("soundtrack.mp3")
-pygame.mixer_music.set_volume(0.5)
-pygame.mixer_music.play(-1)
-
 win = pygame.display.set_mode((1296, 720))
 pygame.display.set_caption("Living Labyrinth")
 clock = pygame.time.Clock()
 background = pygame.image.load('assets/background.jpg').convert()
 walz = pygame.image.load('assets/walls.png')
-
+menuScreen = pygame.image.load('assets/menuScreen.png').convert()
+startButton = pygame.image.load('assets/start.PNG').convert()
 
 class Game(object):
 
@@ -48,6 +45,9 @@ class Game(object):
         self.maze.mazeSkeleton()
         self.maze.fillMaze(0, 0)
         self.maze.scrambleMaze(32, 32)
+        pygame.mixer_music.load("soundtrack.mp3")
+        pygame.mixer_music.set_volume(0.5)
+        pygame.mixer_music.play(-1)
         while self.run:
             clock.tick(30)
             for event in pygame.event.get():
@@ -65,8 +65,46 @@ class Game(object):
             win.fill((0, 0, 0))
             self.redrawGameWindow()
 
-
 game = Game()
-game.start()
+def menuStart():
+    menu = True
+    while menu:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu = False
+            x = pygame.mouse.get_pos()[0]
+            y = pygame.mouse.get_pos()[1]
+            mouse = pygame.mouse.get_pressed()[0]
+            if mouse == 1 and x > 300 and x < 665 and y > 300 and y < 394:
+                game.start()
+                menu = False
+        win.fill((0,0,0))
+        win.blit(menuScreen, [0,0])
+        originalImg = pygame.image.load('assets/button.png').convert()
+        originalImg_rect = pygame.Rect(0, 0, 365, 94)
+        image = pygame.Surface(originalImg_rect.size).convert()
+        image.blit(originalImg, (0, 0), originalImg_rect)
+        alpha = image.get_at((0, 0))
+        image.set_colorkey(alpha)
+        win.blit(image,(250,300))
+        options = pygame.image.load('assets/options.PNG')
+        win.blit(options,(650,300))
+        quit = pygame.image.load('assets/quit.PNG')
+        win.blit(quit,(450,400))
+        if x > 250 and x < 615 and y > 300 and y < 394:
+            pygame.draw.rect(win,(255,0,0),(250,300,365,94),2)
+        if x > 650 and x < 1015 and y > 300 and y < 394:
+            pygame.draw.rect(win,(255,0,0),(650,300,365,94),2)
+        if x > 450 and x < 815 and y >400 and y <494:
+            pygame.draw.rect(win,(255,0,0),(450,400,365,94),2)
+        if x > 250 and x < 615 and y > 300 and y < 394 and mouse == 1:
+            game.start()
+            menu = False
+        if x > 450 and x < 815 and y >400 and y <494 and mouse == 1:
+            menu = False
+        pygame.display.flip()
+        
+menuStart()
+    
 
 pygame.quit()
